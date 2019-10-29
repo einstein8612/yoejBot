ee993d7e75335c1be0765382157abb6812e349c4
einstein8612 <Joey03102004@gmail.com>
2019-10-29 21:33:33 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login("5G2jHKoTeXWpAb1jx9eqFYj5HxWLsxBr");//BOT_TOKEN is the Client Secret
