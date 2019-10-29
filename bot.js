2e90fa58fdb173b141905198f2eb292586a6709a
einstein8612 <Joey03102004@gmail.com>
2019-10-29 21:44:43 +0100

    Y

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login("5G2jHKoTeXWpAb1jx9eqFYj5HxWLsxBr");//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
